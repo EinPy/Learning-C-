525c8d299a6bc96a1e3eaedf68e350289c11e6a4
Einar <einar.bratthall@gmail.com>
2022-03-18 23:01:31 +0100

    Started new project, gitignore not working?

--- PracticeConsoleApplication/PracticeConsoleApplication/Program.cs
+++ PracticeConsoleApplication/PracticeConsoleApplication/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+namespace PracticeConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Second line written and colour changed to green?");
+            Console.Read();
+        }
+    }
+}

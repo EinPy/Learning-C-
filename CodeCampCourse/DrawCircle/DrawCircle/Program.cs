3b04aa64c54a414529ff86f5f9fb621685a88767
Einar Bratthall <einar.bratthall@gmail.com>
2022-05-05 14:51:50 +0200

    made a web todo list with c# and blazor

---
+++ CodeCampCourse/DrawCircle/DrawCircle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawCircle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "Einar";
+            int age;
+            age = 20;
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
